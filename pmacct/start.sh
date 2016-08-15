f5f2f57709058d6270063f537a57b27754f2a605
Johan van den Dorpe <johan.vandendorpe@sohonet.com>
2016-08-15 12:50:33 +0100

    Update pmacct to create rabbitmq exchange early.
    Also provide mechanism for generating sample sflow data.

--- pmacct/start.sh
+++ pmacct/start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+# Create exchange in RabbitMQ early
+while ! nc -z rabbitmq 15672; do   
+  sleep 0.1 # wait for 1/10 of the second before check again
+done
+curl -i -u guest:guest -H "content-type:application/json" -XPUT -d'{"type":"fanout","durable":false}' http://rabbitmq:15672/api/exchanges/%2f/pmacct
+
 /opt/pmacct/sbin/sfacctd -f /opt/pmacct/sfacctd.conf
