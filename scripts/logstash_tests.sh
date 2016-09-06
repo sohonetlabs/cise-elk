0548ec99d9c81f3b0f90b566c1e89a32a681c367
Johan van den Dorpe <johan.vandendorpe@sohonet.com>
2016-09-06 13:52:52 +0100

    Add logstash-tester config via travisci

---
+++ scripts/logstash_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+
+TMPDIR=$(mktemp -d)
+trap "cd && rm -rf $TMPDIR" EXIT
+LSTESTREPO="https://github.com/sohonetlabs/logstash-tester-cise-elk.git"
+
+git clone $LSTESTREPO $TMPDIR
+mkdir -p $TMPDIR/data/config/{conf.d,patterns}
+cat sources/*/filter.conf >> $TMPDIR/data/config/conf.d/filter.conf
+cat sources/syslog/patterns/* >> $TMPDIR/data/config/patterns/patterns
+
+cd $TMPDIR
+./logstash-tester.sh -d data filters
