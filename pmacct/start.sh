41d25b82b4e514bbf90758eee0b45e90a35a65aa
Johan van den Dorpe <johan.vandendorpe@sohonet.com>
2016-09-06 16:13:35 +0100

    Add step to import dummy sflow data record on startup
    This addresses issue #5

--- pmacct/start.sh
+++ pmacct/start.sh
@@ -6,4 +6,8 @@ while ! nc -z rabbitmq 15672; do
 done
 curl -i -u guest:guest -H "content-type:application/json" -XPUT -d'{"type":"fanout","durable":false}' http://rabbitmq:15672/api/exchanges/%2f/pmacct
 
+# Create dummy pmacct data
+curl http://rabbitmq:15672/cli/rabbitmqadmin > /rabbitmqadmin
+cat /event.json | python /rabbitmqadmin -H rabbitmq -u guest -p guest publish exchange=pmacct routing_key="" payload_encoding=""
+
 /usr/bin/supervisord
